974b7ff6d7147fd0a935a4fa00bf1a3260e0beac
MarkID <info@markid.eu>
2020-07-29 22:31:23 +0300

    changed default class name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View } from 'react-native';
 import MarkIDWidget from 'react-native-sdk';
 
-export default class WidgetMinSDK extends React.Component() {
+export default class App extends React.Component() {
    render () {
         return (
             <View>
